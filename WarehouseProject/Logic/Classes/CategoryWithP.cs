8dfb65ce5a115e0cecb88321e4e182b8940350ff
Arno Slabbinck <arno.slabbinck@hotmail.com>
2021-06-10 23:02:17 +0200

    /

--- WarehouseProject/Logic/Classes/CategoryWithP.cs
+++ WarehouseProject/Logic/Classes/CategoryWithP.cs
@@ -8,5 +8,62 @@ namespace WarehouseProject.Logic.Classes
 {
     public class CategoryWithProducts
     {
+        private string categoryName;
+        public string CategoryName
+        {
+            get { return categoryName; }
+            set { categoryName = value; }
+        }
+
+        private string categoryDescription;
+
+        public string CategoryDescription
+        {
+            get { return categoryDescription; }
+            set { categoryDescription = value; }
+        }
+
+        private string productname;
+
+        public string ProductName
+        {
+            get { return productname; }
+            set { productname = value; }
+        }
+
+        private string madin;
+
+        public string MadeIn
+        {
+            get { return madin; }
+            set { madin = value; }
+        }
+
+        private DateTime modelyear;
+
+        public DateTime ModelYear
+        {
+            get { return modelyear; }
+            set { modelyear = value; }
+        }
+
+        private int unitsonorder;
+
+        public int UnitsOnOrder
+        {
+            get { return unitsonorder; }
+            set { unitsonorder = value; }
+        }
+
+
+        private double price;
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+
     }
 }
